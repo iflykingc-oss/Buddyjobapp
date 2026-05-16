88f59a0d2baf0a36fc56449483abb3725558b337
iflykingc-oss <iflykingc@gmail.com>
2026-05-16 10:54:02 +0800

    fix: pasteboard 使用 @ohos.pasteboard 默认导入（SDK 5.0.5.200 不支持 @kit.BasicServicesKit)

--- run_build.bat
+++ run_build.bat
@@ -1,4 +1,4 @@
 @echo off
-set DEVECO_SDK_HOME=E:\HuaweiSDK
+set DEVECO_SDK_HOME=E:\DevEco Studio\sdk\default
 "E:\DevEco Studio\tools\node\node.exe" "E:\DevEco Studio\tools\hvigor\bin\hvigorw.js" --mode module -p module=entry@default -p product=default assembleHap --analyze=normal --parallel > F:\build_out.txt 2>&1
-echo Build finished, exit code: %errorlevel%
+echo exit:%errorlevel% >> F:\build_out.txt
